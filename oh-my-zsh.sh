12224a8beff06ac4c1ea765052528bf05b871479
Andy Li <andy@coglab.biz>
2015-07-09 10:07:19 +0800

    load custom after plugin

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -29,12 +29,6 @@ for config_file ($ZSH/lib/*.zsh); do
   source $config_file
 done
 
-# Load all of your custom configurations from custom/
-for config_file ($ZSH_CUSTOM/*.zsh(N)); do
-  source $config_file
-done
-unset config_file
-
 is_plugin() {
   local base_dir=$1
   local name=$2
@@ -76,6 +70,12 @@ for plugin ($plugins); do
     source $ZSH/plugins/$plugin/$plugin.plugin.zsh
   fi
 done
+
+# Load all of your custom configurations from custom/
+for config_file ($ZSH_CUSTOM/*.zsh(N)); do
+  source $config_file
+done
+unset config_file
 
 # Load the theme
 if [ "$ZSH_THEME" = "random" ]; then
